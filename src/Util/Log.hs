53b8b27df3b7a245ea796e72259d910dda9b05f2
Nutr1t07 <nutr1t07@outlook.com>
2022-03-13 19:39:56 +0800

    feat(Log): log to file

--- src/Util/Log.hs
+++ src/Util/Log.hs
@@ -4,6 +4,7 @@ module Util.Log ( logErr, logWT, logWT'C8, logWT'T, logWT'BL, LogTag(..))where
 import           Data.Time ( defaultTimeLocale, formatTime, getZonedTime )
 import           System.IO ( hFlush, stdout )
 import           Data.Text (Text)
+import           Data.String ( IsString )
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString.Lazy.Char8 as Char8L
 import qualified Data.ByteString.Lazy as BL
@@ -25,13 +26,15 @@ enviroment = "Release"
 data LogTag = Info | Warning | Error | Debug
   deriving (Show, Eq)
 
-log :: Semigroup t => (t -> IO a) -> ([Char] -> t) -> LogTag -> t -> IO ()
-log printFunc packFunc tag msg
+log :: (Semigroup t, IsString t, Show t) => (t -> IO a) -> (FilePath -> t -> IO ()) -> ([Char] -> t) -> LogTag -> t -> IO ()
+log printFunc appendFunc packFunc tag msg
   | tag == Debug && enviroment /= "Debug" = pure ()
   | otherwise = do
       info <- packFunc <$> mkInfo tag
-      printFunc $ info <> msg
+      let logText = info <> msg
+      printFunc logText
       hFlush stdout
+      appendFunc "log.txt" (logText <> "\n")
   where
     mkInfo logTag = do
       t <- formatTime defaultTimeLocale "%Y/%m/%d %H:%M" <$> getZonedTime
@@ -39,16 +42,16 @@ log printFunc packFunc tag msg
 
 -- Log with tag
 logWT :: LogTag -> String -> IO ()
-logWT = log putStrLn id
+logWT = log putStrLn appendFile id
 
 logWT'C8 :: LogTag -> B.ByteString -> IO ()
-logWT'C8 = log Char8.putStrLn Char8.pack
+logWT'C8 = log Char8.putStrLn Char8.appendFile Char8.pack
 
 logWT'BL :: LogTag -> BL.ByteString -> IO ()
-logWT'BL = log Char8L.putStrLn (BL.fromStrict . Char8.pack)
+logWT'BL = log Char8L.putStrLn Char8L.appendFile (BL.fromStrict . Char8.pack)
 
 logWT'T:: LogTag -> Text -> IO ()
-logWT'T = log T.putStrLn T.pack
+logWT'T = log T.putStrLn T.appendFile T.pack
 
 -- | Log errors with action name and details.
 logErr :: String -> String -> IO ()
